023098a77a554f306a783409213f6d6aa8a51553
Mara Diaz <marudiaz0189@gmail.com>
2021-10-14 00:04:57 -0300

    Se agregan mensajes de errores de las validaciones de pantalla contacto.

--- contacto.js
+++ contacto.js
@@ -2,24 +2,31 @@ var nombreYApellido = document.getElementById('nomYAp');
 var Email = document.getElementById('Email');
 var Telefono = document.getElementById('Telefono');
 var Comentario = document.getElementById('Comentario');
+var errorNombreYAp = document.getElementById('errorNombreYAp');
+var errorEmail = document.getElementById('errorEmail');
+var errorTelefono = document.getElementById('errorTelefono');
+var errorComentario = document.getElementById('errorComentario');
+errorNombreYAp.style.color = 'red'
+errorEmail.style.color = 'red'
+errorTelefono.style.color = 'red'
+errorComentario.style.color = 'red'
 
 function enviarFormulario() {
-    var arrayErrores = [];
 
     if (nombreYApellido.value === null | nombreYApellido.value === '') {
-        arrayErrores.push('ingresa tu nombre y apellido');
+        errorNombreYAp.innerHTML = 'ingresa tu nombre y apellido';
     }
 
     if (Email.value === null | Email.value === '') {
-        arrayErrores.push('ingresa tu Email');
+        errorEmail.innerHTML = 'ingresa tu email';
     }
 
     if (Telefono.value === null | Telefono.value === '') {
-        arrayErrores.push('ingresa tu Telefono');
+        errorTelefono.innerHTML = 'ingresa tu telefono';
     }
 
     if (Comentario.value === null | Comentario.value === '') {
-        arrayErrores.push('ingresa tu Comentario');
+        errorComentario.innerHTML = 'ingresa un comentario';
     }
 
     return false;
