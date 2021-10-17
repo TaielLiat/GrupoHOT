843304b0ded4a4d5501150375d7d28bd770414e5
Mara Diaz <marudiaz0189@gmail.com>
2021-10-17 20:39:29 -0300

    se agrega pop up pantalla contacto

--- contacto.js
+++ contacto.js
@@ -12,22 +12,38 @@ errorTelefono.style.color = 'red'
 errorComentario.style.color = 'red'
 
 function enviarFormulario() {
-
+    var mostrarModal = true;
     if (nombreYApellido.value === null | nombreYApellido.value === '') {
         errorNombreYAp.innerHTML = 'ingresa tu nombre y apellido';
+        mostrarModal = false;
     }
 
     if (Email.value === null | Email.value === '') {
         errorEmail.innerHTML = 'ingresa tu email';
+        mostrarModal = false
     }
 
     if (Telefono.value === null | Telefono.value === '') {
         errorTelefono.innerHTML = 'ingresa tu telefono';
+        mostrarModal = false
     }
 
     if (Comentario.value === null | Comentario.value === '') {
         errorComentario.innerHTML = 'ingresa un comentario';
+        mostrarModal = false
+    }
+
+    if(mostrarModal) {
+        modal_container.classList.add('show'); 
     }
 
     return false;
 }
+
+const open = document.getElementById('enviar');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
