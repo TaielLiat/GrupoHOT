aecb743f355d858b8951f229e570797eb653770d
unknown <matigiusti2002@gmail.com>
2021-10-19 17:13:23 -0300

    añado link de pag

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validar()
 
 			if(usuario == "usuario" && Contraseña == "1234")
 			{
-				alert(window.location.href = 'https://www.ole.com.ar')
+				alert(window.location.href = 'file:///E:/Útiles/Visual%20Proyectos/Pruebas%20facu/HOME.html?#')
 			}
 			else
 			{
