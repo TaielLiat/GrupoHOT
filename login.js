0c7e196a078637bf512b4b8da455a798db5024da
unknown <matigiusti2002@gmail.com>
2021-11-03 15:30:44 -0300

    agrego pag individuales de j

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validar()
 
 			if(usuario == "usuario" && Contraseña == "1234")
 			{
-				alert(window.location.href = 'file:///E:/Útiles/Visual%20Proyectos/Pruebas%20facu/HOME.html?#')
+				window.location.href = `./HOME.html`
 			}
 			else
 			{
