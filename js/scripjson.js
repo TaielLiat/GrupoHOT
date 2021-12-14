3e2dc9de657fecffbd48f699dd8368401468794b
unknown <matigiusti2002@gmail.com>
2021-12-14 18:30:36 -0300

    tratando de linkear json

--- js/scripjson.js
+++ js/scripjson.js
@@ -1,19 +1,25 @@
-/------ Parámetros para API ------/
-var data = new FormData();
-data.append('parameter_1', 'value parameter 1');
-data.append('parameter_2', 'value parameter 2');
+const requestURL = 'juegos.json';
+const request = new XMLHttpRequest();
+request.open('GET', url); // setean el método, la url de api
+request.responseType = 'json'; //definen el tipo de dato que les devuelve
+request.send();//envían la solicitud
+var juegos = '';
 
-/------ Parámetros para request ------/
-var xmlhttp = new XMLHttpRequest(),
-    method = 'GET',
-    url    = 'https://developer.mozilla.org/';
+console.log(request.response);
 
-/------ Llamada de la request ------/
-xmlhttp.open(method, url, true);
-xmlhttp.responseType = "json";
 
-/------ Llamada de la request ------/
-xmlhttp.onload = function () {
-    console.log(this.responseText);
-};
-xmlhttp.send(data);
+request.onload = function (){ //esperan la respuesta
+    juegos.log(request.response);
+    equipo = request.response; //reciben la respuesta
+    setDataGames();
+    setTableGammers();
+}
+
+function setDataGames(){
+    let juego = document.getElementById("2");
+    juego.innerHTML = juegos.juego;
+  //  let titulo = document.getElementById("title");
+  //  titulo.innerHTML = "<b>" + equipo["pais"] + "</b>";
+  //  let grupo = document.getElementById("grupo");
+  //  grupo.innerHTML = equipo.grupo;
+}
