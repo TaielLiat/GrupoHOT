424df31588d376a7dcfcb17812b9cfd744e45aad
unknown <matigiusti2002@gmail.com>
2021-12-14 19:35:29 -0300

    intento de logueo con json

--- js/login.js
+++ js/login.js
@@ -1,14 +1,51 @@
-function validar()
-		{
-			var usuario = document.getElementById("usuario").value;
-			var Contraseña = document.getElementById("pass").value;	
+function checkUserLogin(){
+    let username = $('#username').val();
+    let password = $('#password').val();
+    if (username == '' || password == ''){
+        alert('Debe ingresar el usario y la contraseña para continuar');
+    }else{
+        let url = "contraseña.json";
+        $.getJSON( url , function( data ) {
+           // console.log(data);
+            sha256(password).then( function(respuestaHash){
+                checkUser(data, username, respuestaHash);
+               
+            }); 
+                
+        }) 
+        .fail(function() {
+            alert( "error página en mantenimiento " );
+        })
+    }
+}
 
-			if(usuario == "usuario" && Contraseña == "1234")
-			{
-				window.location.href = `./HOME.html`
-			}
-			else
-			{
-				alert("Verifique sus credenciales");
-			}
-		}
+function checkUser(data, username, password){
+        var user = false;
+        $.each( data.users, function( key, val ) {
+      
+            val = JSON.stringify(val);
+            val = JSON.parse(val);
+    
+            if (username === val.user){
+                user = true;
+               
+                if (password == val.password){
+                    console.log( "contraseña Correcta");
+                    $('#userId').val(val.idUser);
+                    $('#login_form').submit();
+                    return false;
+                }else{
+                    alert( "contraseña incorrecta");
+                    return false;
+                }
+            }else {
+                 console.log( "usuario no coincide");
+            }
+        })
+        
+        if (user == false){
+            console.log("Usuario no registrado");
+        }
+       
+   
+}
