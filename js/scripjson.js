7af007d679e4b23f8302dffa00657347952071fe
unknown <matigiusti2002@gmail.com>
2021-12-14 19:03:08 -0300

    .

--- js/scripjson.js
+++ js/scripjson.js
@@ -3,13 +3,13 @@ const request = new XMLHttpRequest();
 request.open('GET', url); // setean el método, la url de api
 request.responseType = 'json'; //definen el tipo de dato que les devuelve
 request.send();//envían la solicitud
-var juegos = '';
+var juegoS = '';
 
 console.log(request.response);
 
 
 request.onload = function (){ //esperan la respuesta
-    juegos.log(request.response);
+    juegoS.log(request.response);
     equipo = request.response; //reciben la respuesta
     setDataGames();
     setTableGammers();
